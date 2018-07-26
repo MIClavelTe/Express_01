99c647108bb1631bd2917bcdbce312aa52b1c02f
MIClavelTe <miclavelte@gmail.com>
2018-07-25 17:14:01 -0700

    Add another route

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const app = express()
 app.get('/', (req,res) => {
     res.send('<h1 style="font-family:Trebuchet MS,Helvetica,sans-serif;color:#a74fff;text-align:center">Hello World!<h1>');
 });
+
+app.get('/hi', (req,res) => {
+    res.send('<h1 style="font-family:Trebuchet MS,Helvetica,sans-serif;color:#4286f4;text-align:center">Hi! Who are you?<h1>');
+});
+
 app.listen(3000);
